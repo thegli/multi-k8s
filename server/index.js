df5b7d985c3dba6317a49824bce6954b5cdcb041
Thomas Egli <thegli.apps@gmail.com>
2019-11-22 22:36:49 +0100

    correct typos

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost Postgres connection'));
 
-// nuber is he Fibonacc index
+// number is the Fibonacc index
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
@@ -51,11 +51,11 @@ app.get('/', (req, res) => {
 
 app.get('/values/all', async(req, res) => {
   const values = await pgClient.query('SELECT * FROM values');
-  // jst return the rows
+  // just return the rows
   res.send(values.rows);
 });
 
-// await not availalbe in Redis client...
+// await not available in Redis client...
 app.get('/values/current', async(req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
